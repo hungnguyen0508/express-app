7b318f8bba2f2f880f30e29bd028c61ca94378bd
hungnguyen0508 <nguyenhuynhquanghung2005@gmail.com>
2025-04-13 15:28:26 +0300

    nextupdate

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World naninani2222222!</h1>')
+  res.send('<h1>Hello World naninani Hưng ơi thành công rồi nè!</h1>')
 })
 
 const PORT = 8080
